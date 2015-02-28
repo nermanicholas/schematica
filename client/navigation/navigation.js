79a02e6e2c95affb889f8cfe35eb001112765a1d
nermanicholas <nerman.nicholas@gmail.com>
2015-02-28 01:09:27 -0500

    files

---
+++ client/navigation/navigation.js
@@ -0,0 +1,9 @@
+Template.navigation.events ({
+  'click .something': function (evt, tmpl) {
+    var name = tmpl.find ('.name').value;
+  }
+});
+
+Template.navigation.firstvar = function () {
+  return "First Var";
+}
